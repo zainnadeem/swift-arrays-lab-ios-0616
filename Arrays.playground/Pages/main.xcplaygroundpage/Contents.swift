7fc26a5ed48df91f5a867de232c3cf291b33ebe3
JimCampagno <jimcampagno@gmail.com>
2016-07-06 15:56:09 -0400

    remove solution

--- Arrays.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ Arrays.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,35 +13,74 @@
  */
 // write your code here
 
+
+
+
+
+
 /*: question2
  ### 2. Write an array called 'shoppingList' **with** a type specified that contains the string values "Bread", "Butter", "Cheese", "Lettuce", "Tomatoes".
  */
 // write your code here
+
+
+
+
 
 /*: question3
  ### 3. Initialize an array called 'futureShoppingList' **with** a type specified and then add the string values "Bread", "Butter", "Cheese", "Lettuce", "Tomatoes".
  */
 // write your code here
 
+
+
+
+
+
 /*: question4
  ### 4. Write an array called 'cheeseSandwich' **with** a type specified whose values will never change and contains the string values "Bread", "Butter", "Cheese", "Lettuce", "Tomatoes".
  */
 // write your code here
+
+
+
+
+
 
 /*: question5
  ### 5. Create a second array called 'desertList' with a type specified using and contains the string values "Cookie dough", "Icecream"
  */
 // write your code here
 
+
+
+
+
+
+
 /*: question6
  ### 6. Write an array called 'afternoonAttendance' **with** a type specified that contains the integer values 2, 10, 3, 15, 7.
  */
 // write your code here
 
+
+
+
+
+
+
+
 /*: question7
  ### 7. Assign the value at the second position in the 'shoppingList' array to a variable called 'itemToPrint' and print it to the screen.
  */
 // write your code here
+
+
+
+
+
+
+
 
 /*: question8
  ### 8. Change the value of 'Cheese' in the 'futureShoppingList' array to 'Chicken' and then print the array to the screen.
@@ -49,21 +88,41 @@
 // write your code here
 
 
+
+
+
+
+
+
 /*: question9
  ### 9. Create a function named 'lifesEssential' that accepts a string of arrays as a parameter called 'ingredients' and returns a boolean. Inside the function check to see if the array passed to the function contains the value "Bread" at position 0. If it does, return a true value boolean, if not false.
  */
 // write your code here
 
-/* question10
+
+
+
+
+
+/*: question10
  ### 10. Pass the 'shoppingList' array to the 'lifesEssential' function and print the result.
  */
 // write your code here
 
-/* question11
+
+
+
+
+
+
+
+/*: question11
  ### 11. Pass the 'desertList' array to the 'lifesEssential' function and print the result.
  */
 // write your code here
 
-/*:
- [Solution](solution)
- */
+
+
+
+
+//: On solution branch, there should be a link here to the solution file.
